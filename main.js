6c990dad58531842a7503569313542206c36e32c
leannecornish-ft <Leanne.Cornish@ft.com>
2020-03-01 21:07:33 +0000

    Changes to input box to add a value and change of button colour on mouse over

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 //main.js
+
+//link the button and change color when roll over
+let button = document.getElementById('button');
+
+button.onmouseover = () => { 
+    button.style.backgroundColor = 'slategray';
+}
+
+button.onmouseout = () => { 
+    button.style.backgroundColor = 'darkslategray';
+}
+
+
+//Removes value from input field when typing 
+let inputField = document.getElementById('input');
+
+inputField.onblur = () => {
+    inputField.value = 'Enter City'
+    inputField.style.color = 'lightgray'
+}
+
+inputField.onfocus = () => {
+    inputField.value = ''
+    inputField.style.color = 'black'
+}
+
+//Hide the weather results secion
+document.getElementById('forecast').hidden = true;
