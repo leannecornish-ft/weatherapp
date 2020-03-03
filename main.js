db7be00ff1ebefbfc30a1c34fdd4a981c77bf726
leannecornish-ft <Leanne.Cornish@ft.com>
2020-03-03 09:45:56 +0000

    fixed user input. API only working on submit when also called directly on page load.

--- main.js
+++ main.js
@@ -1,79 +1,72 @@
 //main.js
 
-//link the button and change color when roll over
+// //link the button and change color when roll over
 let button = document.getElementById('button');
 
-button.onmouseover = () => { 
-    button.style.backgroundColor = 'slategray';
-}
+// button.onmouseover = () => { 
+//     button.style.backgroundColor = 'slategray';
+// }
 
-button.onmouseout = () => { 
-    button.style.backgroundColor = 'darkslategray';
-}
+// button.onmouseout = () => { 
+//     button.style.backgroundColor = 'darkslategray';
+// }
 
 
-//removes value from input field when typing 
-let inputField = document.getElementById('input');
+// //removes value from input field when typing 
+// let inputField = document.getElementById('input');
 
 // inputField.onblur = () => {
-//     inputField.value = 'Enter City'
+//     inputField.value = 'Paris'
 //     inputField.style.color = 'lightgray'
 // }
 
-inputField.onfocus = () => {
-    inputField.value = ''
-    inputField.style.color = 'black'
-}
+// inputField.onfocus = () => {
+//     inputField.value = ''
+//     inputField.style.color = 'black'
+// }
 
 //hide the weather results secion until form submitted
 document.getElementById('forecast').hidden = true;
 
 
+
 //submit 
 
 const weatherKey = '014de69db2b0b78116006b5a3fb13332'
-const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q='
 
-// const getData = async () => {
-//     try {
-//       const city = inputField.value  
-//       const response = await fetch(`${weatherURL}${city}&appid=${weatherKey}`)
-//       console.log(response.ok)
-//       if (response.ok) {
-//         const jsonResponse = await response.json()
-//         return jsonResponse;
-//       }
-//       throw new Error('Request ### failed!')
-//     }
-//     catch(error) {
-//       console.log(error)
-//     }
-//   }
 
 const getData = async () => {
-      const city = "paris"  
-      const response = await fetch(`${weatherURL}${city}&appid=${weatherKey}`)
+  const userText = document.getElementById('input').value  
+  console.log('user text:' + userText)
+  try {
+    const response = await fetch(`${weatherURL}${userText}&appid=${weatherKey}`) 
+    console.log(response.ok) 
+    if (response.ok) {
       const jsonResponse = await response.json()
+      console.log(jsonResponse)
       return jsonResponse;
-      }
+    }
+    throw new Error('Request failed!')
+  }
+  catch(error) {
+    console.log(error)
+    }
+  }
+
+const displayWeather = () => {
+  getData()
+}
+
+button.addEventListener('click', displayWeather) 
+
+getData()
 
 
-getData().then(jsonResponse => console.log(jsonResponse))
-
-//   button.onclick = () => { 
-//       getData()
-    // getData().then(jsonResponse => console.log(jsonResponse))
-//}
 
 
 
-// const getData = () => {
-//     console.log('getData')
-//     const city = inputField.value
-//     const request = `${weatherURL}${city}&appid=${weatherKey}`
-//     fetch(request).then(response => {
-//         if (response.ok) {
-//             console.log(response.json());
-//         }})   
-// }
+
+
+
 
